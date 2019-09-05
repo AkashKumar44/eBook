1efbfa7a08b3ce03eb7d7292e21cc42904f6a48d
Akash_Kumar <akashgola44579@gmail.com>
2019-09-05 13:19:23 +0530

    Create vedioup.php

---
+++ css/vedioup.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>vedio</title>
+</head>
+<body>
+		<form method="post" enctype="multipart/form-data"> 
+			<table border="1" style="padding:10px">
+				<tr><Td>Upload  Video</td></tr>
+ 				<tr><td><input type="file" name="fileToUpload"/></td></tr>
+				<tr>
+					<td>
+						<input type="submit" value="Uplaod Video" name="upd"/>
+						<input type="submit" value="Display Video" name="disp"/>
+ 					</td>
+ 				</tr>
+ 			</table>
+ 		</form>
+	
+</body>
+</html>
